e19d2d32bac1200e5bacc35373c6116681ccd132
jellycodes <jylee7942@gmail.com>
2022-11-24 09:48:49 +0900

    1124

--- vscode/pages/board/index.jsx
+++ vscode/pages/board/index.jsx
@@ -1,15 +1,6 @@
 import React from 'react'
 import Board from '../../components/Board/Board'
 
-<<<<<<< HEAD
-const boardPage = () => {
-  return (
-    <Board />
-  )
-}
-
-export default boardPage
-=======
 const BoardPage = () => {
   return (
     <Board/>
@@ -17,4 +8,3 @@ const BoardPage = () => {
 }
 
 export default BoardPage
->>>>>>> eb6c05b91feaf6e2e1490fc562299f05a00f935f
